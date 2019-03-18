b1190110f57e9a2012c581847dda710467a5697b
spduk <spdevuk@gmail.com>
2019-03-18 09:02:14 +0000

    feat: add  optional wallpaper setting on complete and aspect ratios

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,25 @@
 # frozen_string_literal: true
 
 require "json"
-require "erb"
 require "fileutils"
 require "mechanize"
 require "cgi"
 require "tty/prompt"
 require "tty/spinner"
-prompt = TTY::Prompt.new(enable_color: true)
 
+prompt = TTY::Prompt.new(enable_color: true)
 # used to avoid repeat images
 File.write("history.json", {}.to_json) unless File.file?("history.json")
 $history = JSON.parse(File.read("history.json"))
 $save_path = "/home/#{ENV['USER']}/Pictures/Wallpapers/"
 
-def search(query, categories, nsfw, atleast, pages)
+def search(query, categories, nsfw, ratios, atleast, pages)
   agent = Mechanize.new
   enc_query = CGI.escape(query)
 
+  # todo: scrape the amount of maximum pages before starting, then go up to pages or the max amount
   1.upto(pages) do |page|
-    url = "https://alpha.wallhaven.cc/search?q=#{enc_query}&categories=#{categories}&page=#{page}&purity=#{nsfw}&atleast=#{atleast}&sorting=relevance&order=desc"
+    url = "https://alpha.wallhaven.cc/search?q=#{enc_query}&categories=#{categories}&page=#{page}&purity=#{nsfw}&ratios=#{ratios}&atleast=#{atleast}&sorting=relevance&order=desc"
 
     agent.get(url).links_with(class: /preview/).each do |base|
       id = base.href[/\d+/]
@@ -46,7 +46,7 @@ end
 
 
 # questions to know what to search for
-query = prompt.ask("What would you like to search for?")
+query = prompt.ask("What would you like to search for?").capitalize
 
 get_categories = prompt.multi_select("Which categories?") do |menu|
   %w(general anime people)
@@ -55,7 +55,8 @@ get_categories = prompt.multi_select("Which categories?") do |menu|
   menu.choice :anime, 1
   menu.choice :people, 2
 end
-
+# categories are a multi-select but .multi_select doesn't return an array of the same size
+# so we have to figure out which ones go where and replace the 0s if they should be replaced
 categories = ["0", "0", "0"]
 get_categories.each { |i| categories[i] = "1" }
 categories = categories.join("")
@@ -68,17 +69,30 @@ nsfw = prompt.select("Would you like to find potentially NSFW images?") do |menu
   menu.choice :both, "110"
 end
 
+
+ratio_options = %w(4x3 16x9 16x10 32x9 48x9 9x16 10x16)
+ratios = prompt.multi_select("Which aspect ratios? Default finds them all", ratio_options).join("%2C")
+
 resolutions = %w(1280x720 1600x900 1920x1080 2560x1440 3840x2160)
 atleast = prompt.select("What is the minimum resolution you'd like to find?", resolutions)
 
-pages = prompt.slider("How many pages would you like to find?", min: 1, max: 20, step: 1)
+pages = prompt.slider("How many pages would you like to find?", min: 1, max: 20, default: 1, step: 1)
 
 # start search spinner and begin
 spinner = TTY::Spinner.new("[:spinner] Searching for #{query}...", format: :dots)
 spinner.auto_spin
 setup_folder()
-search(query, categories, nsfw, atleast, pages)
+search(query, categories, nsfw, ratios, atleast, pages)
+
 
 # open the folder when done
-spinner.success("Done! Files saved in /#{$save_path}#{query}")
-`nautilus #{$save_path}/#{query}`
+files = Dir["#{$save_path}#{query}/*"]
+if files.length > 0
+  spinner.success("Done! #{files.length} Files saved in file://#{$save_path}#{query}")
+  if prompt.yes?("Would you like to set a background using one of the images you just downloaded?")
+    `gsettings set org.gnome.desktop.background picture-uri file://#{files.sample}`
+  end
+  prompt.say("[✔] Finished!")
+else
+  spinner.error("Could not find any wallpapers for #{query} with those options")
+end
